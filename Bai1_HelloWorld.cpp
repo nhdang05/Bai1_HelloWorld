a185a0fe3460f9322de4a6ceffa318b9c7f159c2
nhdang <MAY 06@LAB2-08>
2024-11-04 11:24:37 +0700

    Add project files.

---
+++ Bai1_HelloWorld.cpp
@@ -0,0 +1,11 @@
+// Bai1_HelloWorld.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+int main() {
+	cout << "Hello World!\n";
+	cout << "toi ten la_" << "\t Nguyen Hai Dang" << endl;
+	cout << "toi dang hoc \tC++" << endl;
+	return 0;
+}
